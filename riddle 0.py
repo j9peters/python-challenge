9268b8f649e2545e97a903ba7d25786dda08809a
j9peters <jeanine.peters@asu.edu>
2017-10-10 12:29:10 -0700

    Riddle 0 solution.

---
+++ riddle 0.py
@@ -0,0 +1,12 @@
+# Riddle 0
+# URL:  http://www.pythonchallenge.com/pc/def/0.html
+
+def evaluate(base, exponent):
+    """Evaluate exponential expression and return result."""
+    return base**exponent
+
+print(evaluate(2,38))
+
+# Output:  274877906944
+
+# URL to Riddle 1:  http://www.pythonchallenge.com/pc/def/274877906944.html
